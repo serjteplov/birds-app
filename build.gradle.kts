afba713cc9fef426863946058d9d5ff4a01b13d4
Sergey Teplov <serj.teplov>
2023-01-19 07:04:28 +0300

    m1l3 changes in gradle scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,28 +4,21 @@ val appCurrentVersion = project.properties["birds.app.currentVersion"] as String
 val appJvmVersion = project.properties["birds.app.jvmTarget"] as String
 
 plugins {
-    kotlin("jvm") version Version.KOTLIN apply false
+    kotlin("jvm")
 }
 
+group = "ru.serj"
+version = Version.BIRDS_APP
+
 allprojects {
-    group = "ru.serj"
-    version = Version.BIRDS_APP
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = Version.JVM_TARGET
-    }
     repositories {
         mavenCentral()
     }
-}
-
-repositories {
-    appJvmVersion
-    mavenCentral()
 }
 
 subprojects {
-    repositories {
-        mavenCentral()
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = Version.JVM_TARGET
     }
 }
 
