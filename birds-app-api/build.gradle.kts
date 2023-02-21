a86e6dd460e39289132a3d94faebd4a96c19a8eb
Sergey Teplov <serj.teplov>
2023-02-21 21:55:33 +0300

    m2l5 add openapi spec.yml

---
+++ birds-app-api/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+// создать саму спеку
+// сгенерировать классы по ней
+// написать тесты сериализации на Котесте
---
+++ birds-app-common/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+// подключить модуль api
+// написать внутренние модели
+// создать мапперы
+// создать тесты на мапперы на Котесте
