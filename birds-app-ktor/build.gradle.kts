85e910913437288d0db835acf38df8744391de53
Sergey Teplov <serj.teplov>
2023-03-18 13:06:13 +0300

    m2l16 init ktor

--- birds-app-ktor/build.gradle.kts
+++ birds-app-ktor/build.gradle.kts
@@ -1,9 +1,23 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
 
 plugins {
     kotlin("jvm")
 }
 
 dependencies {
+
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-config-yaml-jvm:$ktor_version")
+
+
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
     implementation(kotlin("stdlib"))
     implementation(project(":birds-app-api-v1"))
---
+++ birds-app-ktor/src/main/kotlin/Application.kt
@@ -0,0 +1,8 @@
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
