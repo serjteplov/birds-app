bd0eb78b909055f81f18e512dcb1048f0e97dea9
Sergey Teplov <serj.teplov>
2023-10-11 06:40:34 +0300

    task 1 up jvm to 17 in github Actions

---
+++ birds-app-spring-webflux/src/main/kotlin/ru/serj/birds/reactive/controller/MyController.kt
@@ -0,0 +1,47 @@
+package ru.serj.birds.reactive.controller
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.reactor.flux
+import kotlinx.coroutines.reactor.mono
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import ru.serj.api.v1.models.Error
+import ru.serj.api.v1.models.ResponseResult
+import ru.serj.api.v1.models.TweetFilterRequest
+import ru.serj.api.v1.models.TweetFilterResponse
+
+@RestController
+class MyController {
+
+    @PostMapping("/bird/v2/flux")
+    suspend fun getFiltered(req: TweetFilterRequest): Flux<TweetFilterResponse> {
+        val resp1 = TweetFilterResponse(
+            requestId = "requestId1",
+            result = ResponseResult.SUCCESS
+        )
+        val resp2 = TweetFilterResponse(
+            requestId = "requestId2",
+            result = ResponseResult.SUCCESS
+        )
+        val resp3 = TweetFilterResponse(
+            requestId = "requestId3",
+            result = ResponseResult.SUCCESS
+        )
+        val flux = Flux.just(resp1, resp2, resp3)
+        return flux
+    }
+    @PostMapping("/bird/v2/mono")
+    suspend fun getFilteredMono(req: TweetFilterRequest): Mono<TweetFilterResponse> {
+        val resp1 = TweetFilterResponse(
+            requestId = "requestId1",
+            result = ResponseResult.SUCCESS
+        )
+        return mono {
+            delay(3000)
+            resp1
+        }
+    }
+}
